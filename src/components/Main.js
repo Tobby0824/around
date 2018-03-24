32e780097e3e30f366c0f25a1e4922aa451646e8
Tobby0824 <taoxuan0824@gmail.com>
2018-03-24 19:27:16 -0400

    fix routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,21 +2,30 @@ import React from 'react';
 import {Register} from "./Register";
 import {Login} from "./Login";
 import {Home} from './Home';
-import { Switch, Route } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router';
 
 export class Main extends React.Component {
     getLogin = () => {
-        return this.props.isLoggedIn? <Home /> : <Login handleLogin={this.props.handleLogin}/>;
+        return this.props.isLoggedIn? <Redirect to="/home" /> : <Login handleLogin={this.props.handleLogin}/>;
+    }
+
+    getRoot = () => {
+        return <Redirect to="/login"/>;
+    }
+
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to="/login"/>;
     }
 
     render() {
         return(
             <div className="main">
                 <Switch>
-                    <Route exact path="/" render={this.getLogin}/>
+                    <Route exact path="/" render={this.getRoot}/>
                     <Route path="/login" render={this.getLogin}/>
                     <Route path="/register" component={Register}/>
-                    <Route render={this.getLogin}/>
+                    <Route path="/home" render={this.getHome} />
+                    <Route render={this.getRoot}/>
                 </Switch>
             </div>
         );
