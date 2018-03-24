b997ffe41672db9aef1f7245145bbffa4ceccce9
Tobby0824 <taoxuan0824@gmail.com>
2018-03-24 19:49:49 -0400

    add logout

--- src/components/App.js
+++ src/components/App.js
@@ -12,13 +12,19 @@ class App extends Component {
 
     handleLogin = (token) => {
         localStorage.setItem(TOKEN_KEY, token);
-        this.setState({isLoggedIn:true});
+        this.setState({isLoggedIn: true});
     }
+
+    handleLogout = () => {
+        this.setState({isLoggedIn: false});
+        localStorage.removeItem(TOKEN_KEY);
+    }
+
   render() {
     return (
       <div className="App">
-          <Header/>
-          <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin}/>
+          <Header isLoggedIn={this.state.isLoggedIn} handleLogout={this.handleLogout}/>
+          <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin} />
         <p className="App-intro">
         </p>
       </div>
